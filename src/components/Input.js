e0635c709426c53a25f7265fe1f345385f5b44aa
onurrsalmann <salmannonur@gmail.com>
2020-08-08 14:45:53 +0300

    toDoApp

--- src/components/Input.js
+++ src/components/Input.js
@@ -15,7 +15,7 @@ const Input = (props) => (
             width: width * 0.9,
             height: height * 0.08,
             backgroundColor: '#f5f5f5',
-            color:'#ffffff',
+            color:'#000000',
             borderRadius: 5,
             borderWidth:0.5,
             borderColor:'#000000',
